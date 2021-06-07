a1e24dfc8cccd11aa618a8e20c55c793ec9b9efd
Yondo202 <yondooo61@gmail.com>
2021-06-07 12:13:33 +0800

    dd

--- src/global/axiosbase.js
+++ src/global/axiosbase.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://pimis-plan.edp.mn/pimis-edplan/",
+  baseURL: "https://pimis-plan.edp.mn/pimis-edplan/",
+  // baseURL: "http://localhost:1337/pimis-edplan/",
   // baseURL: "http://192.168.88:230:1337/pimis-edplan/",
 });
 
 export default instance;
 
 export const axiosGraphql = axios.create({
-  baseURL: "http://pimis-plan.edp.mn/",
+  baseURL: "https://pimis-plan.edp.mn/",
 });
--- src/global/edpAxios.js
+++ src/global/edpAxios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://pimis.edp.mn/api/",
-  // baseURL: "http://localhost:3000/api/",
 });
 
 export default instance;
