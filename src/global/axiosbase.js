87ad34da5ebdba6bf301996dfd08725a863be33f
Yondo202 <yondooo61@gmail.com>
2022-04-13 09:34:37 +0800

    dd

--- src/global/axiosbase.js
+++ src/global/axiosbase.js
@@ -4,8 +4,8 @@ const instance = axios.create({
   // baseURL: "https://pimis-plan.edp.mn/pimis-edplan/", // undsen backend ed-plan - ii
 
   // baseURL: "http://localhost:1337/pimis-edplan/",
-  baseURL: "http://192.168.88.232:1337/pimis-edplan/",
-  // baseURL: "https://3a15-124-158-107-34.ngrok.io/pimis-edplan/",
+  // baseURL: "http://192.168.88.232:1337/pimis-edplan/",
+  baseURL: "https://9831-124-158-107-34.ngrok.io/pimis-edplan/",
 });
 
 export default instance;
@@ -14,4 +14,3 @@ export const axiosGraphql = axios.create({
   // baseURL: "https://pimis-plan.edp.mn/",  // undsen backend ed-plan - ii axiosbase
   baseURL: "http://localhost/",
 });
-
